7f82ba30c4cc2254684fcd7c4e9d0f1a33e92ebc
martin_chakarov <martin.d.chakarov@gmail.com>
2022-11-06 01:13:40 +0200

    Add die component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import Die from './components/Die';
+
 export default function App() {
 
   return (
     <main className="app">
+      <div className="dice">
+        <Die value={1}/>
+        <Die value={2}/>
+        <Die value={3}/>
+        <Die value={4}/>
+        <Die value={5}/>
+        <Die value={6}/>
+        <Die value={1}/>
+        <Die value={2}/>
+        <Die value={3}/>
+        <Die value={4}/>
+      </div>
     </main>
   );
 }
